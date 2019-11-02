c688922e410a35084313cb87b7a8ddd2f6e5cbde
Jokius <djok69@gmail.com>
2019-11-02 10:27:04 +0300

    Add responds and respond_json helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+
+  def responds(interactor, input, &block)
+    input = input.to_unsafe_hash if input.is_a? ActionController::Parameters
+    interactor.new.call(input) { |result| result_processing(result, &block) }
+  end
+
+  def respond_json(json)
+    respond_to do |format|
+      format.html { render template: 'default/stub' }
+      format.json { render json }
+    end
+  end
+
+  private
+
+  def result_processing(result, &block)
+    result.success do |value|
+      return yield(value) if block
+
+      respond_json json: value
+    end
+
+    result.failure do |error|
+      respond_json(json: error[:message], status: (error[:status] || 400))
+    end
+  end
 end
