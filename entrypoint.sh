5a25609b9a4033fe6d9ae38ff1c3690fb40b6a81
Jokius <djok69@gmail.com>
2019-12-11 09:12:31 +0300

    Change production config (#17)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env sh
 
 function app {
-    ./bin/rails s -b 0.0.0.0 "$@"
+  bundle
+  bundle exec rails credentials:edit
+  bundle exec rails assets:precompile
+  bundle exec rails db:prepare
+  rm -rf node_modules
+  bundle exec rails s -b 0.0.0.0 "$@"
 }
 
 function rake {
-    bundle exec rails "$@"
+  bundle exec rails "$@"
+}
+
+function rails {
+  bundle exec rails "$@"
 }
 
 case $1 in
-    app) shift && app $@;;
-    rake) shift && rake $@;;
-    *) echo "Cannot find command" && exit 1;;
+  app) shift && app $@;;
+  rake) shift && rake $@;;
+  rails) shift && rails $@;;
+  *) echo "Cannot find command" && exit 1;;
 esac
