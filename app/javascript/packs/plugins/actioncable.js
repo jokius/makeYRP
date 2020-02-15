126838f5fd7b9061cc2aa81da4cdea2ac343c36a
Jokius <djok69@gmail.com>
2020-02-16 00:16:33 +0300

    Change get host

--- app/javascript/packs/plugins/actioncable.js
+++ app/javascript/packs/plugins/actioncable.js
@@ -2,11 +2,10 @@ import ActionCableVue from 'actioncable-vue'
 import Vue from 'vue'
 
 (function() {
-  const host = `${window.location.hostname}:${window.location.port}`
   Vue.use(ActionCableVue, {
     debug: true,
     debugLevel: 'error',
-    connectionUrl: `ws://${host}/cable`,
+    connectionUrl: `ws://${window.location.host}/cable`,
     connectImmediately: true,
   })
 }())
