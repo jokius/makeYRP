f675cc888c7b241633df868f8c1a6bb931647c15
Jokius <djok69@gmail.com>
2020-04-24 10:42:27 +0300

    Add rolls

---
+++ app/javascript/packs/components/Templates/components/pbta/EdgeOfUniverse/chat/EofRoll.vue
@@ -0,0 +1,19 @@
+<template>
+  <auto-full v-if="body.autoFull" :body="body" />
+  <auto-part v-else-if="body.autoPart" :body="body" />
+  <dice-roll v-else :body="body" />
+</template>
+
+<script>
+  import AutoFull from './AutoFull'
+  import AutoPart from './AutoPart'
+  import DiceRoll from './DiceRoll'
+
+  export default {
+    name: 'EofRoll',
+    components: { DiceRoll, AutoPart, AutoFull },
+    props: {
+      body: { type: Object, required: true },
+    },
+  }
+</script>
