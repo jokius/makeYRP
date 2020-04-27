14660a6f929855c3c905d4ab9ff09d393ee4beb8
Jokius <djok69@gmail.com>
2020-04-27 23:47:23 +0300

    Add specials tab

--- app/javascript/packs/components/Templates/components/pbta/EdgeOfUniverse/sheets/CharacterSheet.vue
+++ app/javascript/packs/components/Templates/components/pbta/EdgeOfUniverse/sheets/CharacterSheet.vue
@@ -12,6 +12,9 @@
       <v-tab class="tab" active-class="tab-active">
         <span class="xolonium-font">Ходы</span>
       </v-tab>
+      <v-tab v-for="item in specialTabs" :key="`tabs-${item.index}`" class="tab" active-class="tab-active">
+        <span class="xolonium-font">{{ item.tab.name }}</span>
+      </v-tab>
       <v-tab class="tab" active-class="tab-active">
         <span class="xolonium-font">Снаряжение</span>
       </v-tab>
@@ -27,6 +30,9 @@
       <v-tab-item>
         <character-move-body :id="id" />
       </v-tab-item>
+      <v-tab-item v-for="item in specialTabs" :key="`tab-item-${item.index}`">
+        <tab-special :id="id" :special="item.tab" :special-index="item.index" />
+      </v-tab-item>
       <v-tab-item>
         <character-equipment-body :id="id" />
       </v-tab-item>
@@ -38,14 +44,17 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   import CharacterMainBody from './CharacterMainBody'
   import CharacterMoveBody from './CharacterMoveBody'
   import CharacterRelationshipBody from './CharacterRelationshipBody'
   import CharacterEquipmentBody from './CharacterEquipmentBody'
+  import TabSpecial from './TabSpecial'
 
   export default {
     name: 'CharacterSheet',
-    components: { CharacterEquipmentBody, CharacterRelationshipBody, CharacterMoveBody, CharacterMainBody },
+    components: { TabSpecial, CharacterEquipmentBody, CharacterRelationshipBody, CharacterMoveBody, CharacterMainBody },
     props: {
       id: { type: Number, required: true },
       size: { type: Object, required: true },
@@ -54,10 +63,21 @@
     data() {
       return {
         tab: null,
+        specialTabs: [],
       }
     },
 
     computed: {
+      ...mapState({
+        sheets: state => state.game.sheets,
+      }),
+
+      sheet: {
+        get() {
+          return this.sheets.find(sheet => sheet.id === this.id)
+        },
+      },
+
       width: {
         get() {
           const width = this.size.width
@@ -71,6 +91,15 @@
           return typeof height === 'number' ? `${height}px` : height
         },
       },
+    },
+
+    created() {
+      this.specialTabs = []
+      this.sheet.params.specials.forEach((item, index) => {
+        if (item && item.type === 'tab') {
+          this.specialTabs.push({ index: index, tab: item })
+        }
+      })
     },
   }
 </script>
