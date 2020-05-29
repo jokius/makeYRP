f016b63fa6af8b1dea2ad2accd32e422d41e9d35
Jokius <djok69@gmail.com>
2020-05-29 22:46:04 +0300

    Remove frontend

--- frontend/lib/dateTime.js
+++
@@ -1,7 +0,0 @@
-const toLocaleTime = date => new Date(date).toLocaleTimeString()
-const toLocaleDate = date => new Date(date).toLocaleDateString()
-
-export {
-  toLocaleTime,
-  toLocaleDate,
-}
--- frontend/plugins/dragDrop.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueDragDrop from 'vue-drag-drop'
-
-if (process.client) Vue.use(VueDragDrop)
