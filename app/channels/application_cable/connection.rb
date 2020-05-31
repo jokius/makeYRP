476150053e63e7efd367bee962a05cd67513c57d
Jokius <djok69@gmail.com>
2020-06-01 02:05:45 +0300

    Fix sockets auth

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,8 +11,13 @@ module ApplicationCable
     private
 
     def find_verified_user
-      verified_user = User.find_by(id: cookies.signed['user.id'])
+      verified_user = User.find_by(jti: jwt['jti'])
       verified_user || reject_unauthorized_connection
+    end
+
+    def jwt
+      token = request.params[:token].split(' ')[1]
+      JWT.decode(token, Rails.application.credentials.jwt_secret, true, algorithm: 'HS256', verify_jti: true)[0]
     end
   end
 end
