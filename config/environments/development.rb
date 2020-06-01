a83f606a7700c1d618fa180bcd8a433c07ff7bda
Jokius <djok69@gmail.com>
2020-06-01 05:49:21 +0300

    Change dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,6 @@ Rails.application.configure do
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.active_storage.service = :local
+
+  config.action_cable.disable_request_forgery_protection = true
 end
