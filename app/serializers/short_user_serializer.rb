684b74fb96840acacf32aaf393a3ad98de9bdbc5
Jokius <djok69@gmail.com>
2020-09-25 04:20:02 +0300

    Add color to serializer

--- app/serializers/short_user_serializer.rb
+++ app/serializers/short_user_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ShortUserSerializer < BaseSerializer
-  attributes :nickname, :admin
+  attributes :nickname, :admin, :color
   attribute :avatar, &:avatars
 
   attribute :sheet do |user, params|
